3bf3315dbbcded607aee1197aa8a00f27be61e0d
romati <rt-61@bk.ru>
2024-11-22 01:38:44 +0400

    hello from bot

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello world!")
+import requests
+import json
+
+TOKEN = '7642633597:AAH_6GDWxeNVSUYtg0IcqdET-n3-Q-Xsd-A'
+
+URL = 'https://api.telegram.org/bot{TOKEN}/{method}'
+
+updates = 'getUpdates'
+send = 'sendMessage'
+
+data = {
+    'chat_id': '56140962',
+    'text': 'hello from bot',
+    }
+
+url = URL.format(TOKEN=TOKEN, method=send)
+
+
+# response = requests.get(url)
+response = requests.post(url, data=data)
+json_content = json.loads(response.text)
+
+
+print(response)
