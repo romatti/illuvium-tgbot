a654c7ec89422e30c404606e4c5997de8bc1268c
romati <rt-61@bk.ru>
2024-11-23 16:08:09 +0400

    aiogram

--- main.py
+++ main.py
@@ -1,10 +1,60 @@
 import const
 import config
+import http.client
+import asyncio
+import logging
+import sys
+
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+TOKEN = config.TOKEN
+dp = Dispatcher()
 
 
-def main():
-    pass
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
 
 
-if __name__ == '__main__':
-    main()
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    """
+    Handler will forward receive a message back to the sender
+
+    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    """
+    try:
+        # Send a copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Nice try!")
+
+
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+    # conn = http.client.HTTPSConnection("api.immutable.com")
+    # headers = {'Accept': "application/json"}
+    # conn.request("GET",
+    #              "/v1/assets?user=0x3bb7a999016cb246e2aa0925838a39451e3759d2&collection=0x205634b541080afff3bbfe02dcc89f8fa8a1f1d4",
+    #              headers=headers)
+    # res = conn.getresponse()
+    # data = res.read()
+    # print(data.decode("utf-8"))
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
